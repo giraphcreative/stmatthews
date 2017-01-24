7d81bf32344aa0f4b8b7aade25530ce79e5eda73
James Pederson <james@jpederson.com>
2017-01-23 20:36:02 -0600

    Update analytics UA ID.

--- js/src/main.js
+++ js/src/main.js
@@ -53,6 +53,6 @@ jQuery(document).ready(function($){
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-47451257-1', 'auto');
+ga('create', 'UA-38315794-42', 'auto');
 ga('send', 'pageview');
 
